df9404298fb89f85a376284cbad7857e5818f4b2
Juan Edi <jedi@manas.com.ar>
2015-10-14 15:36:52 -0300

    Only use SO_REUSEPORT if supported by kernel.
    Otherwise, try to bind socket and retry if it port is already in used.

--- lib/instedd_telemetry/api_server.rb
+++ lib/instedd_telemetry/api_server.rb
@@ -7,9 +7,13 @@ module InsteddTelemetry
     end
 
     def run
-      init_socket
-
       until should_stop? do
+        until try_init_socket do
+          InsteddTelemetry::Logging.log :warn, "Could not start remote api server. Will retry in 1 minute."
+          sleep 1.minute
+        end
+
+        InsteddTelemetry::Logging.log :info, "Remote api server successfully started on port #{InsteddTelemetry.configuration.api_port}"
         Thread.start(@socket.accept) do |client, client_sockaddr|
           handle(client)
         end
@@ -45,9 +49,23 @@ module InsteddTelemetry
       @should_stop
     end
 
+    def try_init_socket
+      begin
+        init_socket
+        true
+      rescue
+        false
+      end
+    end
+
     def init_socket
       @socket = Socket.new Socket::AF_INET, Socket::SOCK_STREAM, 0
-      @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEPORT, true)
+
+      # allow other daemons to bind to this port if inactive (avoids "address already in use" issues after a recent crash)
+      @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+      
+      # if kernel supports it, allow multiple daemons to attach to the same port.
+      @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEPORT, true) if reuse_port?
 
       sockaddr = Socket.pack_sockaddr_in(InsteddTelemetry.configuration.api_port, '0.0.0.0')
       @socket.bind(sockaddr)
@@ -55,5 +73,10 @@ module InsteddTelemetry
 
       @should_stop = false
     end
+
+    def reuse_port?
+      Socket.constants.include? :SO_REUSEPORT
+    end
+
   end
 end
