9004572f9187771f7c0825b9bc2879ced2ded085
Adrian Romero <aromero@manas.com.ar>
2015-11-12 14:28:28 -0300

    Changed migration to be compatible with rails 3.2

--- db/migrate/20151112170121_remove_period_from_timespans.rb
+++ db/migrate/20151112170121_remove_period_from_timespans.rb
@@ -1,5 +1,9 @@
 class RemovePeriodFromTimespans < ActiveRecord::Migration
-  def change
-    remove_column :instedd_telemetry_timespans, :period_id, :integer
+  def up
+    remove_column :instedd_telemetry_timespans, :period_id
+  end
+
+  def down
+    add_column :instedd_telemetry_timespans, :period_id, :integer
   end
 end
