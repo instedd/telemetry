cc25f9b8bf6cbafa92f9012c72a9da7e5675d392
Adrian Romero <aromero@manas.com.ar>
2015-12-22 12:53:17 -0300

    Fixed constant not found

--- lib/instedd_telemetry/api.rb
+++ lib/instedd_telemetry/api.rb
@@ -1,25 +1,27 @@
-class InsteddTelemetry::Api
-  def initialize(base_uri)
-    @base_uri = URI(base_uri)
-  end
+module InsteddTelemetry
+  class Api
+    def initialize(base_uri)
+      @base_uri = URI(base_uri)
+    end
 
-  def create_event(event)
-    req = Net::HTTP::Post.new("/api/v1/installations/#{InsteddTelemetry.instance_id}/events", {'Content-Type' =>'application/json'})
-    req.body = event.to_json
+    def create_event(event)
+      req = Net::HTTP::Post.new("/api/v1/installations/#{InsteddTelemetry.instance_id}/events", {'Content-Type' =>'application/json'})
+      req.body = event.to_json
 
-    perform(req)
-  end
+      perform(req)
+    end
 
-  def update_installation(installation_params)
-    req = Net::HTTP::Put.new("/api/v1/installations/#{InsteddTelemetry.instance_id}", {'Content-Type' =>'application/json'})
-    req.body = installation_params.to_json
+    def update_installation(installation_params)
+      req = Net::HTTP::Put.new("/api/v1/installations/#{InsteddTelemetry.instance_id}", {'Content-Type' =>'application/json'})
+      req.body = installation_params.to_json
 
-    perform(req)
-  end
+      perform(req)
+    end
 
-  protected
+    protected
 
-  def perform(request)
-    Net::HTTP.new(@base_uri.hostname, @base_uri.port).request(request)
+    def perform(request)
+      Net::HTTP.new(@base_uri.hostname, @base_uri.port).request(request)
+    end
   end
 end
